237a03d34a1e66ad5a98011ab2468b258f166117
Frederik Salling Østergaard <au588968@uni.au.dk>
2021-07-31 20:20:26 +0200

    Test again

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.use(express.static('build'))
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '../../public');
+const port = process.env.PORT || 8080;
+app.use('/static', express.static(publicPath));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+    console.log('Server is up!');
+});
